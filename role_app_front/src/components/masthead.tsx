ce1c70ff5ff978dca5d598153fcf84c902902101
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2022-08-01 21:47:43 -0300

    principal banner made

---
+++ role_app_front/src/components/masthead.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+function Masthead() {
+    return (
+        <section className="flex flex-col min-h-screen min-w-[100vw] justify-center bg-masthead bg-cover">
+            <div>
+                <h1 className="font-bold text-yellow-400">Rolê App</h1>
+                <br /><br />
+                <p className="max-w-[900px] text-center my-0 mx-auto">
+                    Rolê App é um lugar pra você que procura o rolê perfeito para seu dia. Acesse nosso app e encontre o melhor local para você e seus amigos curtirem!
+                    <br /><br />
+                    Navegue entre as centenas de opções: bares, pubs, restaurantes e conforme sua preferência, faça sua reserva e curta sua noite!
+                </p>
+            </div>
+        </section >
+    )
+
+}
+
+export default Masthead
--- role_app_front/tailwind.config.cjs
+++ role_app_front/tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'masthead': "url('src/assets/masthead_bg.jpg')",
+      }
+    },
   },
   plugins: [],
 }
